a878b87458ec9aa1b86cf0af87dd1dd19d343afe
Sholde <exshode@gmail.com>
2021-02-03 20:47:04 +0100

    add free information
    - non free information don't work as I want

--- compilation/alloc/alloc_debugger.c
+++ compilation/alloc/alloc_debugger.c
@@ -8,16 +8,22 @@
 
 unsigned long long nb_alloc = 0;
 unsigned long long size_alloc = 0;
+
 unsigned long long nb_free = 0;
+unsigned long long size_free = 0;
 
 void *(*malloc_func)(size_t) = NULL;
 void (*free_func)(void *) = NULL;
 
+alloc_table_t table;
+
 void *malloc(size_t size)
 {
+  // Allocation variable incrementation
   nb_alloc++;
   size_alloc += size;
 
+  // First call
   if (!malloc_func)
     {
       malloc_func = (void *(*)(size_t))dlsym(RTLD_NEXT, "malloc");
@@ -27,15 +33,37 @@ void *malloc(size_t size)
           abort();
         }
     }
+
+  // Call true malloc
+  void *ptr = malloc_func(size);
+  if (!ptr)
+    return NULL;
   
-  return malloc_func(size);
+  // Stock allocation information
+  if (table.count < MAX_ALLOC)
+    {
+      for (int i = 0; i < MAX_ALLOC; i++)
+        {
+          if (table.ptr[i] == NULL)
+            {
+              table.count++;
+              table.ptr[i] = ptr;
+              table.size[i] = size;
+              break;
+            }
+        }
+    }
+
+  // Return pointer
+  return ptr;
 }
 
 void free(void *ptr)
 {
+  // Free variable incremantation
   nb_free++;
-  unsigned long long size_alloc = 0;
 
+  // First call
   if (!free_func)
     {
       free_func = (void (*)(void *))dlsym(RTLD_NEXT, "free");
@@ -46,11 +74,55 @@ void free(void *ptr)
         }
     }
 
-    free_func(ptr);
+  // Stock free information
+  if (table.count)
+    {
+      for (int i = 0; i < MAX_ALLOC; i++)
+        {
+          if (table.ptr[i] == ptr)
+            {
+              table.count--;
+              size_free += table.size[i];
+              table.ptr[i] = NULL;
+              table.size[i] = 0;
+              break;
+            }
+        }
+    }
+
+  // Call true free
+  free_func(ptr);
+}
+
+__attribute((constructor)) void init(void)
+{
+  table.count = 0;
+  for (int i = 0; i < MAX_ALLOC; i++)
+    {
+      table.ptr[i] = NULL;
+      table.size[i] = 0;
+    }
 }
 
 __attribute((destructor)) void finalize(void)
 {
-  printf("Allocate byte : %8.lld (%8.lld call)\n", size_alloc, nb_alloc);
-  printf("Free byte     : %8s (%8.lld call)\n", "NULL", nb_free);
+  // Display general information
+  printf("Allocate : %8.lld Bytes (%8.lld call)\n", size_alloc, nb_alloc);
+  printf("Free     : %8.lld Bytes (%8.lld call)\n", size_free, nb_free);
+
+  // Display non free memory
+  if (table.count)
+    {
+      printf("WARNING: NON FREE MEMORY\n");
+      for (int i = 0; i < MAX_ALLOC; i++)
+        {
+          if (table.ptr[i])
+            {
+              printf("Non Free : %8.lld Bytes (NOT call)\n", table.size[i]);
+              table.ptr[i] = NULL;
+              table.size[i] = 0;
+              table.count--;
+            }
+        }
+    }
 }
--- compilation/alloc/alloc_debugger.h
+++ compilation/alloc/alloc_debugger.h
@@ -1,8 +1,18 @@
 #ifndef _ALLOC_DEBUGGER_H_
 #define _ALLOC_DEBUGGER_H_
 
+#define MAX_ALLOC 20
+
+typedef struct alloc_table_s
+{
+  size_t count;
+  void *ptr[MAX_ALLOC];
+  size_t size[MAX_ALLOC];
+} alloc_table_t;
+
 void *malloc(size_t size);
-//void free(void *ptr);
+void free(void *ptr);
+__attribute((constructor)) void init(void);
 __attribute((destructor)) void finalize(void);
 
 #endif
